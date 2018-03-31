58782fdb5d791443b03ab48fac6d22dbe97e9202
Imagination <vladislavkv@bk.ru>
2018-03-31 11:33:31 +0600

    add new database

--- lesson5/lesson5.py
+++ lesson5/lesson5.py
@@ -1,17 +1,35 @@
-file = open('database.txt', 'r', encoding = 'utf-8')
+#Shop database
+#file = open ('database.txt', 'r', encoding = 'utf-8')
 
-leastProduct = 9999
-leastLine = [ ]
+#leastProduct = 9999
+#leastLine = [ ]
 
-for line in file:
-    valuesList = line.split(',')
+#for line in file:
+#    valuesList = line.split (',')
 
-    if int (valuesList[4]) < leastProduct:
-        leastProduct = int (valuesList[4])
-        leastLine = valuesList
+#    if int (valuesList[4]) < leastProduct:
+#        leastProduct = int (valuesList[4])
+#        leastLine = valuesList
         
-print ('Меньше всего товара под названием:', leastLine[1])
-print ('Остаток:', leastLine[4])
+#print ('Меньше всего товара под названием:', leastLine[1])
+#print ('Остаток:', leastLine[4])
+
+#
+personInfo ={
+              'виктор':'+7(***)***9052',
+              'рахат':'+7(***)****1265',
+              'максим':'+7(***)***7542',
+              'олег':'+7(***)***2374'
+            }
+
+personName = input ('Введите имя: ')
+personName = personName.lower()
+
+if personName in personInfo:
+    print (personInfo[personName])
+else:
+    print ('Ошибка! (Такого имени нет)')
+
 
 
     
