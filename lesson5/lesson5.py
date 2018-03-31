d3ccdd8825b627863384c91d935cb4ddb29f1287
Imagination <vladislavkv@bk.ru>
2018-03-31 11:00:48 +0600

    work with files

--- lesson5/lesson5.py
+++ lesson5/lesson5.py
@@ -1,1 +1,20 @@
+file = open('database.txt', 'r', encoding = 'utf-8')
+
+leastProduct = 9999
+leastLine = [ ]
+
+for line in file:
+    valuesList = line.split(',')
+
+    if int (valuesList[4]) < leastProduct:
+        leastProduct = int (valuesList[4])
+        leastLine = valuesList
+        
+print ('Меньше всего товара под названием:', leastLine[1])
+print ('Остаток:', leastLine[4])
+
+
+    
+
+
 
